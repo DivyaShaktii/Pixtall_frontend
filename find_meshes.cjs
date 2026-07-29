a98db82d6a1601156b30c4e601751d0e0c84c8f5
Rajashekar H S <rajashekarhs10@gmail.com>
2026-07-29 19:02:23 +0530

    Update .gitignore to exclude AI agent configuration folders and scratch directories

--- find_meshes.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const { parse } = require('@loaders.gl/core');
-const { GLTFLoader } = require('@loaders.gl/gltf');
-const THREE = require('three');
-
-// We need a DOM-less Three.js GLTF parser.
-// Actually, it's easier to just write a temporary React component to log the meshes!
